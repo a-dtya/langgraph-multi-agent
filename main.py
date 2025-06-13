dde19807c302800c48bf9f429c6ea7a7bbe60e56
a-dtya <adithyavinod1943@gmail.com>
2025-06-14 00:11:10 +0530

    basic graph with one node created

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from typing import TypedDict, Dict
 from langgraph.graph import StateGraph #blueprint
 
 
-class AgentState(TypedDict): #agentstate keeps track of information as application runs
+class AgentState(TypedDict): #agentstate keeps track of information as application runs (state schema)
     message: str
-
-
 
 def greeting_node(state: AgentState) -> AgentState:
     """Node that adds greeting message to the state"""
     state["message"] = f"Hello {state['message']}, how are you?"
     return state
-    
+
+graph = StateGraph(AgentState)
+graph.add_node("greeting", greeting_node)
+
+graph.set_entry_point("greeting")
+graph.set_finish_point("greeting")
+
+app = graph.compile()
+
+result = app.invoke({"message": "John"})
+
+print(result["message"])
