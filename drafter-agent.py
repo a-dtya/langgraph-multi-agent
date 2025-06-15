486d9a05615b8bfd12fa772a4858a0b43115c135
a-dtya <adithyavinod1943@gmail.com>
2025-06-15 23:04:05 +0530

    added save tool, init agent node

--- drafter-agent.py
+++ drafter-agent.py
@@ -24,6 +24,66 @@ def update(content: str) -> str:
     document_content = content
 
     return f"Document updated successfully. The current document content is: \n{document_content}"
+
+#save tool is called, towards conversation completion to save draft into a txt file
+
+@tool
+def save(filename: str) -> str:
+    """ Save the current document to a text file and finish the process. 
     
+    Args:
+        filename (str): The name of the text file to save the document to.
+
+    """ 
+    global document_content
+
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+        
+    try:
+        with open(filename, "w") as f:
+            f.write(document_content)
+        print(f"Success: Document saved successfully to {filename}")
+        return f"Success: Document saved successfully to {filename}"
+    except Exception as e:
+        return f"Error: Failed to save document: {str(e)}"
+
+
+tools = [save, update]
+
+model = ChatOpenAI(model="gpt-4o-mini").bind_tools(tools)
+
+def our_agent(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(content=f"""
+        You are Drafter, a helpful writing assistant. You are going to help the user update and modify documents.
+        
+        - If the user wants to update or modify content, use the 'update' tool with the complete updated content.
+        - If the user wants to save and finish, you need to use the 'save' tool.
+        - Make sure to always show the current document state after modifications.
+        
+        The current document content is:{document_content}
+        """)
+    if not state["messages"]:
+        user_input = "I will help you to update a document. What would you like to create?"
+        user_message = HumanMessage(content=user_input)
+
+    else:
+        user_input = input("\nWhat would you like to do with the document? ")
+        
+        print(f"User: {user_input}")
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state["messages"]) + [user_message]
+
+    response = model.invoke(all_messages)
+    
+    
+
+        
+
+
+
+
+
     
     
