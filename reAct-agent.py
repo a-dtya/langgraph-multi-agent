f2f64d3738ab997f605a04ebdfc269b56f2fd8d6
a-dtya <adithyavinod1943@gmail.com>
2025-06-15 17:20:42 +0530

    added reAct agent with conditional tool calling

--- reAct-agent.py
+++ reAct-agent.py
@@ -7,10 +7,10 @@ from langchain_core.messages import SystemMessage, BaseMessage, ToolMessage # ba
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END #blueprint
 from langgraph.graph.message import add_messages
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
 
 load_dotenv()
-
-llm = ChatOpenAI(model="gpt-4o-mini")
 
 #add_messages is a reducer function
 #it tells us how to merge data into the current state
@@ -27,5 +27,60 @@ llm = ChatOpenAI(model="gpt-4o-mini")
 # update = {"messages": ["hello"]}
 # new_state = {"messages": ["hi", "hello"]} append is not an option in complex workflows
 
+class AgentState(StateGraph):
+    messages: Annotated[Sequence[BaseMessage],add_messages]
 
+@tool
+def add(a: int, b: int):
+    """This tool adds two numbers"""
+    return a + b
+
+tools = [add]
+model = ChatOpenAI(model="gpt-4o-mini").bind_tools(tools)
+
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(content="You are a helpful assistant. Please answer to my query.")
+
+    response = model.invoke([system_prompt]+ state["messages"])
+
+    return {"messages": [response]} # here the reducer fn handles the appending to state["messages"]
+
+
+#conditional edge
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools=tools)
+graph.add_node("tool_node", tool_node)
+
+graph.add_edge(START, "our_agent")
+graph.add_conditional_edges("our_agent", should_continue, {
+    "continue": "tool_node",
+    "end": END
+})
+
+graph.add_edge("tool_node", "our_agent")
+
+app = graph.compile()
+
+inputs = {"messages": [("user", "What is 1 + 1?")]}
+
+result = app.invoke(inputs)
+
+print(result["messages"])
+
+
+
+
+
+    
+     
 
