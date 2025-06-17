721283609bad99dcec5f5a4841d564818fe5a730
a-dtya <adithyavinod1943@gmail.com>
2025-06-18 02:42:04 +0530

    updated agent with negotiator node

--- negotiator-agent-2.py
+++ negotiator-agent-2.py
@@ -15,5 +15,47 @@ load_dotenv()
 currentPriceList = [{"item":"tomato","rate":25},{"item":"potato","rate":35}]
 targetPriceList = [{"item":i["item"], "rate":int(i["rate"]*0.8)} for i in currentPriceList]
 
-print("Current Price List: ", currentPriceList)
-print("Target Price List: ", targetPriceList)
+# print("Current Price List: ", currentPriceList)
+# print("Target Price List: ", targetPriceList)
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    currentPriceList: List[Dict[str, Union[str, int]]]
+    targetPriceList: List[Dict[str, Union[str, int]]]
+    
+
+def negotiator(state: AgentState) -> AgentState:
+    
+    system_prompt = SystemMessage(content=f"""
+    You are a helpful assistant. You are going to negotiate prices of all items from user.
+    The current price list is: {state["currentPriceList"]}
+    The target price list is: {state["targetPriceList"]}
+    You are going to ask supplier to reduce prices to target prices.
+
+    - call the update_current_price tool every time you get the price of an item which is lesser than it's rate in {state["currentPriceList"]}
+    - Make sure to show the current state of prices after every update
+    - make only ONE tool call at a time
+    """)
+    # print(f"\nState: {state["messages"]}")
+    if not state["messages"]:
+        user_input = f"Try negotiating prices of all items in {state["currentPriceList"]} to target prices in {state["targetPriceList"]}"
+        user_message = HumanMessage(content=user_input)
+
+    else:
+        #check if the last message is of type ToolMessage, if so, skip the user input
+        if isinstance(state["messages"][-1], ToolMessage):
+            user_input = ""
+        else:
+            user_input = input("\nYour response: ")
+        
+        print(f"User: {user_input}")
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state["messages"]) + [user_message]
+
+    response = model.invoke(all_messages)
+
+    print("\nAssistant: ", response.content)
+    # print("\n Price List: ", state["price"])
+
+    return {"messages": list(state["messages"]) + [user_message, response]}
